88c3854ebc2033c6e7d1dd101d3db9e3ffbf3da9
Tegan Gallacher <tegan4956@hotmail.com>
2017-01-24 16:50:46 +0000

    geolocation added

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
 
+
 var initialize = function() {
   var centre = {lat: 51.5074, lng: -0.1278};
   var sherlock = {lat:51.523772, lng: -0.158538};
   var mapDiv  = document.querySelector('#main-map');
-  var mainMap = new MapWrapper(mapDiv, centre, 12);
+  var mainMap = new MapWrapper(mapDiv, centre, 10);
 
   mainMap.addMarker(centre);
   mainMap.addInfoMarker(sherlock); 
@@ -11,10 +12,25 @@ var initialize = function() {
 
   var setMapCenter = function() {
     mainMap.googleMap.setCenter({lat: -50.607113, lng: 165.972561});
+    mainMap.googleMap.setZoom(12);
   };
 
   var buttonClick = document.querySelector('#city-button');
   buttonClick.onclick = setMapCenter;
+
+  var geoButtonClick = document.querySelector('#current-location');
+  console.log(geoButtonClick);
+  geoButtonClick.onclick = function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    console.log(pos);
+    mainMap.googleMap.setCenter(pos);
+    mainMap.googleMap.setZoom(15);
+  })
+};
 
 };
 
