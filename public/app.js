c226b0bef269da368482075aad0152d2f19c1565
Tegan Gallacher <tegan4956@hotmail.com>
2017-01-24 20:16:44 +0000

    added more stuff

--- public/app.js
+++ public/app.js
@@ -18,20 +18,50 @@ var initialize = function() {
   var buttonClick = document.querySelector('#city-button');
   buttonClick.onclick = setMapCenter;
 
+  var setToMistake = function() {
+    mainMap.googleMap.setCenter({lat: 44.474802, lng: -67.534991});
+    mainMap.googleMap.setZoom(15);
+  };
+
+  var mistakeClick = document.querySelector('#mistake-button');
+  mistakeClick.onclick = setToMistake;
+
+  var setToLonely = function() {
+    mainMap.googleMap.setCenter({lat: 45.562828, lng: -81.465340});
+    mainMap.googleMap.setZoom(12);
+  };
+
+  var lonelyClick = document.querySelector('#lonely-button');
+  lonelyClick.onclick = setToLonely;
+
   var geoButtonClick = document.querySelector('#current-location');
   console.log(geoButtonClick);
   geoButtonClick.onclick = function() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    console.log(pos);
-    mainMap.googleMap.setCenter(pos);
-    mainMap.googleMap.setZoom(15);
-  })
-};
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log(pos);
+      mainMap.googleMap.setCenter(pos);
+      mainMap.googleMap.setZoom(15);
+    })
+  };
 
 };
+
+function flashtext(element,colour) {
+  var colourCheck = document.getElementById(element).style.color;
+
+    if (colourCheck === 'black') {
+      document.getElementById(element).style.color = colour;
+    } else {
+      document.getElementById(element).style.color = 'black';
+    }
+  }
+
+  setInterval(function() {
+    flashtext("heading",'gold');
+  }, 350 );
 
 window.onload = initialize;
